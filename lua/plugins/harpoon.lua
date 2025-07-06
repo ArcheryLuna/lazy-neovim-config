21d8d85778f6114097642ccf0d3f41f676c1f094
ArcheryLuna <me@archery-luna.com>
2025-07-06 14:35:39 +0100

    feat: Harpoon rebinds

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -10,26 +10,27 @@ return {
 		vim.keymap.set("n", "<leader>a", mark.add_file, {
 			desc = "Add file to harpoon",
 		})
+
 		vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu, {
 			desc = "Toggle harpoon menu",
 		})
 
-		vim.keymap.set("n", "<C-h>", function()
+		vim.keymap.set("n", "<C-1>", function()
 			ui.nav_file(1)
 		end, {
 			desc = "Navigate to harpoon file 1",
 		})
-		vim.keymap.set("n", "<C-t>", function()
+		vim.keymap.set("n", "<C-2>", function()
 			ui.nav_file(2)
 		end, {
 			desc = "Navigate to harpoon file 2",
 		})
-		vim.keymap.set("n", "<C-n>", function()
+		vim.keymap.set("n", "<C-3>", function()
 			ui.nav_file(3)
 		end, {
 			desc = "Navigate to harpoon file 3",
 		})
-		vim.keymap.set("n", "<C-s>", function()
+		vim.keymap.set("n", "<C-4>", function()
 			ui.nav_file(4)
 		end, {
 			desc = "Navigate to harpoon file 4",
---
+++ lua/plugins/zenmode.lua
@@ -0,0 +1,41 @@
+return {
+	"folke/zen-mode.nvim",
+	keys = {
+		{
+			"<leader>zz",
+			function()
+				require("zen-mode").setup({
+					window = {
+						width = 90,
+						options = {},
+					},
+				})
+				require("zen-mode").toggle()
+
+				vim.wo.wrap = false
+				vim.wo.number = true
+				vim.wo.rnu = true
+			end,
+			mode = "n",
+			desc = "Zen mode",
+		},
+		{
+			"<leader>zZ",
+			function()
+				require("zen-mode").setup({
+					window = {
+						width = 80,
+						options = {},
+					},
+				})
+				require("zen-mode").toggle()
+				vim.wo.wrap = false
+				vim.wo.number = false
+				vim.wo.rnu = false
+				vim.opt.colorcolumn = "0"
+			end,
+			mode = "n",
+			desc = "zen mode no numbers",
+		},
+	},
+}
