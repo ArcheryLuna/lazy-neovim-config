d4d8afffcca5f3a6f414e27c6c62573976738f84
ArcheryLuna <me@archery-luna.com>
2025-07-06 14:21:40 +0100

    feat: Working mappings

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -4,7 +4,7 @@ return {
 		{ "<leader>gs", vim.cmd.Git, desc = "Open Git Status" },
 		{ "<leader>ga", "<cmd>Git add .<cr>", desc = "Git add" },
 		{ "<leader>gc", "<cmd>Git commit<cr>", desc = "Git commit" },
-		{ "<leader>gp", "<cmd>Git push<cr>", desc = "Git push" },
+		{ "<leader>gg", "<cmd>Git push<cr>", desc = "Git push" },
 		{ "<leader>gu", "<cmd>Git pull<cr>", desc = "Git push" },
 	},
 }
